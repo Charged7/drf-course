0906e0aff509c6843bb7de7a8f76938944e83dee
Char <hubaibohdan258@gmail.com>
2026-03-01 18:02:50 +0200

    fix redis url for render

--- Video1/drf_course/settings.py
+++ Video1/drf_course/settings.py
@@ -160,19 +160,22 @@ SPECTACULAR_SETTINGS = {
 }
 
 #==========================
+import os
+
+REDIS_URL = os.environ.get("REDIS_URL", "redis://redis:6379/1")
+
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://redis:6379/1",
+        "LOCATION": REDIS_URL,
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
     }
 }
 
-#==========================
-CELERY_BROKER_URL = "redis://redis:6379/1"
-CELERY_RESULT_BACKEND = "redis://redis:6379/1"
+CELERY_BROKER_URL = REDIS_URL
+CELERY_RESULT_BACKEND = REDIS_URL
 
 # EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
